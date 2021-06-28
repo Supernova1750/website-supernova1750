62d5fa88f96d1464396bf19e5d8a6f515f091eed
Ben Klingenburg <bek6692@thi.de>
2021-06-29 00:11:42 +0200

    testing sql fetch in authenticate.php

--- login/authenticate.php
+++ login/authenticate.php
@@ -3,9 +3,65 @@ session_start();
 
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
   header("location: ../welcome");
+  exit;
 }
-$usr = 'supernova';
-$pwd = '3838bd5806d32cd91144865aa822b9551417dd2796c163d390baa7074d3067a7';
+
+require_once '../php/config.php';
+
+$usr = $pwd = '';
+$usr_err = $pwd_err = $login_err = '';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(empty(trim($_POST['username']))) {
+        $usr_err = 'Please enter username';
+    } else {
+        $usr = trim($_POST['username']);
+    }
+
+    if(empty(trim($_POST['password']))) {
+        $usr_err = 'Please enter password';
+    } else {
+        $usr = trim($_POST['password']);
+    }
+
+    if(empty($usr_err) && empty($pwd_err)) {
+        $sql = 'SELECT id, username, password, FROM users WHERE username = ?';
+
+        if($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, 's', $param_usr);
+
+            $param_usr = $username;
+
+            if(mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+
+                if(mysqli_stmt_num_rows($stmt) == 1) {
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+
+                            header('location: ../welcome');
+                        } else {
+                            $login_err = 'Invalid username or password!';
+                        }
+                    }
+                } else {
+                    $login_err = 'Invalid username or password!';
+                }
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+    mysqli_close($link);
+}
 
 if($_POST['username'] == $usr && hash('sha256', $_POST['password']) == $pwd) {
   echo 'correct';
