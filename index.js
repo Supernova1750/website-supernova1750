7160919b909cc2700a94c7a2b73430bc5b6cf4bd
Ben Klingenburg <bek6692@thi.de>
2021-06-28 23:42:14 +0200

    undo changes

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logo from './logo.svg';
-import 'bootswatch/dist/slate/bootstrap.min.css'; // Added this :boom:
-import './App.css';
-
-function index() {
-  return '<h1>h</h1>'
-}
