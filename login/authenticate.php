bccc98016c4f895bab6018cfbffe12034bea17bb
Ben Klingenburg <bek6692@thi.de>
2021-06-28 23:34:47 +0200

    Changed index.php to index.html

--- login/authenticate.php
+++ login/authenticate.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-if(!isset($_POST['username'],$_POST['password'])) {
-  exit('Please fill in your username and password');
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+  header("location: ../welcome");
 }
 $usr = 'supernova';
 $pwd = '3838bd5806d32cd91144865aa822b9551417dd2796c163d390baa7074d3067a7';
@@ -11,4 +12,4 @@ if($_POST['username'] == $usr && hash('sha256', $_POST['password']) == $pwd) {
 } else {
   echo 'no, try again';
 }
- ?>
+?>
