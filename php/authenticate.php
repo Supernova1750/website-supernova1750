b6d760cd66707f39c2862f441c1103b43bb36482
Ben Klingenburg <bek6692@thi.de>
2021-06-29 12:21:26 +0200

    testing sql fetch in authenticate.php

--- php/authenticate.php
+++ php/authenticate.php
@@ -35,9 +35,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             if(mysqli_stmt_execute($stmt)) {
                 mysqli_stmt_store_result($stmt);
+                echo 'test before username check' . '<br>';
                 if(mysqli_stmt_num_rows($stmt) == 1) {
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-
+                    echo 'test before fetch' . '<br>';
                     if(mysqli_stmt_fetch($stmt)){
                         echo 'test before password check' . '<br>';
                         if(password_verify($password, $hashed_password)){
