f92a65f04889a029911b8f66d5995c126f1d5f6d
Ben Klingenburg <bek6692@thi.de>
2021-06-29 15:29:17 +0200

    added auto-redirect on login page

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    header('location: /welcome');
-    echo 'testloggedin';
+    header('location: welcome');
     exit;
 } else {
-    header('location: /login');
-    echo 'testnotloggedin';
+    header('location: login');
     exit;
 }
 ?>
