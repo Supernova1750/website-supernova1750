4a5739a7333d132cc3186a0393b0fda0201a981f
Ben Klingenburg <bek6692@thi.de>
2021-06-30 01:27:54 +0200

    removed debug tools

--- php/config.php
+++ php/config.php
@@ -1,8 +1,5 @@
 <?php
-# TODO: REMOVE!!!!!!!!!!
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
