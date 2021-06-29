f71421ba977e7aed1577e919befd7d56ae040aa6
Ben Klingenburg <bek6692@thi.de>
2021-06-29 12:04:19 +0200

    testing sql fetch in authenticate.php

--- php/config.php
+++ php/config.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'dRDbQrzyrF7L');
+define('DB_USERNAME', 'website-user');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'website');
  
 /* Attempt to connect to MySQL database */
@@ -13,4 +13,6 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+echo $link;
 ?>
