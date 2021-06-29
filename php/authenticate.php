73548d557fad547d51a89d57a9fe882d62a11bc6
Ben Klingenburg <bek6692@thi.de>
2021-06-29 14:56:29 +0200

    testing sql fetch in authenticate.php

--- php/authenticate.php
+++ php/authenticate.php
@@ -43,7 +43,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
-                            header('location: ../welcome');e
+                            header('location: ../welcome');
                             exit;
                         } else {
                             $login_err = 'Invalid username or password!';
