60287a3f78d37b58abb32eea26ae3fadc86427c5
Ben Klingenburg <bek6692@thi.de>
2021-06-29 14:38:50 +0200

    testing sql fetch in authenticate.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     header('welcome');
+    echo 'testloggedin';
     exit;
 } else {
     header('login');
+    echo 'testnotloggedin';
     exit;
 }
 ?>
