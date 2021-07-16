7ff45100f8374abf0f274349ec49453a8bd62334
Ben Klingenburg <bek6692@thi.de>
2021-07-16 20:10:18 +0200

    improving Homepage

--- homepage/index.php
+++ homepage/index.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == false) {
     header("Location: ../login");
     die();
 }
-
+include "../php/config.php";
 ?>
 
 <html lang="en">
@@ -18,16 +18,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == false) {
     <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../bootstrap/themes/darkly/bootstrap.min.css">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap/themes/darkly/bootstrap.min.css">
     <script src="../bootstrap/js/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="../bootstrap/js/bootstrap.min.js"></script>
 
-    <script src="like.js"></script>
+    <script src="index.js"></script>
 </head>
 <body>
-<div id="#like_button_container"></div>
+<h1 class="header"><?php echo $_SESSION["username"]?></h1>
 </body>
 </html>
 
