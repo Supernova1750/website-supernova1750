735c8150e1c6230e4873f23f1f122cd6db387ac7
Ben Klingenburg <bek6692@thi.de>
2021-07-21 15:46:03 +0200

    oopsie

---
+++ main/php/authenticate.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+  header("Location: /homepage");
+  die;
+}
+
+include '../php/config.php';
+
+$usr = $pwd = '';
+$usr_err = $pwd_err = $login_err = '';
+
+if(empty($link)) {
+    $link = '';
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(empty(trim($_POST['username']))) {
+        $usr_err = 'Please enter username';
+    } else {
+        $usr = trim($_POST['username']);
+    }
+
+    if(empty(trim($_POST['password']))) {
+        $pwd_err = 'Please enter password';
+    } else {
+        $pwd = trim($_POST['password']);
+    }
+
+    if((empty($usr_err) && empty($pwd_err)) || $link != false) {
+        $sql = 'SELECT id, username, password FROM users WHERE username = ?';
+        $stmt = mysqli_prepare($link, $sql);
+        if($stmt) {
+            mysqli_stmt_bind_param($stmt, 's', $param_usr);
+
+            $param_usr = $usr;
+            if(mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1) {
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($pwd, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+                            header('Location: /homepage');
+                        } else {
+                            $login_err = 'Invalid username or password!';
+                        }
+                    }
+                } else {
+                    $login_err = 'Invalid username or password!';
+                }
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    } else {
+       echo $pwd_err . '<br>';
+    }
+    mysqli_close($link);
+}
+?>
---
+++ main/php/register.php
@@ -0,0 +1,50 @@
+<?php
+include '../php/config.php';
+
+$reg_err = '';
+
+if(empty($link)) {
+    $link = '';
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $usr = trim($_POST['username']);
+    $pwd = trim($_POST['password']);
+    if(empty(trim($usr))) {
+    } else if(strlen($usr) < 3 || strlen($usr) > 15) {
+        $reg_err = 'Username has to be between 3 and 15 letters';
+    } else if(!preg_match('/^(\w){3,15}$/', $usr)) {
+        $reg_err = 'Username cannot contain special characters';
+    } else if($pwd != trim($_POST['password-repeat'])) {
+        $reg_err = 'Passwords do not match.';
+    } else if(empty($pwd) ||  strlen($pwd) < 7) {
+        $reg_err = 'Password has to be at least 8 characters long';
+    } else if($stmt = mysqli_prepare($link,'SELECT id FROM users WHERE username = ?')) {
+        mysqli_stmt_bind_param($stmt, 's', $param_usr);
+        $param_usr = $usr;
+        $reg_err = 'test1';
+        if(mysqli_stmt_execute($stmt)) {
+            $reg_err = 'test2';
+            mysqli_stmt_store_result($stmt);
+            if(mysqli_stmt_num_rows($stmt) >= 1) {
+                $reg_err = 'Username is already taken';
+            } else {
+                $reg_err = 'test3';
+                if($stmt = mysqli_prepare($link, 'INSERT INTO users (username, password) VALUES (?,?)')) {
+                    $reg_err = 'test4';
+                    mysqli_stmt_bind_param($stmt, 'ss', $param_usr, $param_pwd);
+                    $param_usr = $usr;
+                    $param_pwd  = password_hash($pwd, PASSWORD_DEFAULT);
+                    if(mysqli_stmt_execute($stmt)) {
+                        header("Location: /login");
+                    }
+                    mysqli_stmt_close($stmt);
+                }
+            }
+        }
+
+        mysqli_stmt_close($stmt);
+    }
+}
+
+?>
